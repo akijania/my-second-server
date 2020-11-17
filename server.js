16553ec2b7c3e422eb404114eb0d8eabf88d00d0
Anna Kijania <kijania.anna@gmail.com>
2020-11-17 22:39:18 +0100

    Add server

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.show = (name) => {
+    res.sendFile(path.join(__dirname, `/views/${name}`));
+  };
+  next();
+});
+
+app.use('/user', (req, res, next) => {
+  res.send('You need to log in!');
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.show('index.html');
+});
+
+app.get('/home', (req, res) => {
+  res.show('index.html');
+});
+
+app.get('/about', (req, res) => {
+  res.show('about.html');
+});
+
+app.get('/user/settings', (req, res) => {
+  res.show('user/settings.html');
+});
+
+app.get('/user/panel', (req, res) => {
+  res.show('user/panel.html');
+});
+
+app.get('/404.jpg', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/404.jpg'));
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, '/views/error.html'));
+});
+
+app.listen(7000, () => {
+  console.log('Server is running on port: 7000');
+});
